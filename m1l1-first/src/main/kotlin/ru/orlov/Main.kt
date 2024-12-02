f59129bf5dacbd45039b6ae193f3fd54d32dc9bc
Dmitriy <ddd12@mail.ru>
2024-12-02 11:01:35 +0300

    hw01

---
+++ m1l1-first/src/main/kotlin/ru/orlov/Main.kt
@@ -0,0 +1,5 @@
+package ru.orlov
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ m1l1-first/src/test/kotlin/ru/orlov/MainKtTest.kt
@@ -0,0 +1,11 @@
+package ru.orlov
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MainKtTest {
+    @Test
+    fun testFirst() {
+        assertEquals("hello kotlin!", "hello kotlin!")
+    }
+}
